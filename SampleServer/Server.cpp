8700fceeee1be1a6bbccf01bd289fc4a069fcbf9
KillerDucks <nimesh.patel@bindserver.com>
2018-06-25 15:38:34 +0100

    Added a way to remove the padding using memset and memcpy

--- SampleServer/Server.cpp
+++ SampleServer/Server.cpp
@@ -7,6 +7,8 @@
 using namespace ReallySimpleProtocolServer;
 using namespace ReallySimpleProtocol;
 
+using namespace std;
+
 int main()
 {
 	int number = 5;
@@ -15,15 +17,27 @@ int main()
 	const char* test128 = "Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello World 12345";
 	Packet RCP;
 	const int packetNum = (const int)RCP.CalcPacketSize((char *)test, packetLength);
-	std::vector<Packet::DataPacket> packets;
-	std::cout << RCP.FlagDebug((Packet::PacketFlags::Flags)(Packet::PacketFlags::NullData | Packet::PacketFlags::TestFlag)) << std::endl;
-	std::cout << "Packets Need for given data -> " << RCP.CalcPacketSize((char *)test, packetLength) << std::endl;
-	std::cout << "Packet Data Length (Total) -> " << packetLength << std::endl;
+	vector<Packet::DataPacket> packets;
+	//cout << RCP.FlagDebug((Packet::PacketFlags::Flags)(Packet::PacketFlags::NullData | Packet::PacketFlags::TestFlag)) << endl;
+	cout << "Packets Need for given data -> " << RCP.CalcPacketSize((char *)test, packetLength) << endl;
+	cout << "Packet Data Length (Total) -> " << packetLength << endl;
 	for (size_t i = 0; i < packetNum; i++)
 	{
 		packets.emplace_back(RCP.CreatePackets(128, (Packet::PacketFlags::Flags)(Packet::PacketFlags::NullData | Packet::PacketFlags::TestFlag), test128));
 	}
-	std::cout << "Vector Size -> " << packets.size() << std::endl;
+	cout << "Vector Size -> " << packets.size() << endl;
+
+	// Debug Printing
+	for (size_t i = 0; i < packets.size(); i++)
+	{
+		printf_s("Packet %d Data -> %s\n", i, packets[i].data.c_str());
+		printf_s("Packet %d Flags -> %s\n", i, RCP.FlagsString(packets[i].flags).c_str()); // Wont print !
+		printf_s("Packet %d Packet Length -> %d\n", i, packets[i].packetLength);
+	}
+
+	//// Test with baked in server
+	//SocketServer server;
+	//server.StartServer();
 	system("pause");
 	return 1;
 }
